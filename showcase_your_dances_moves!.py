3d63d902803ae9266e18b0df2da87b277db450ed
Hfairbanks <Hydeia.fairbanks9620@gmail.com>
2024-11-20 16:15:11 -0500

    first commit

---
+++ showcase_your_dances_moves!.py
@@ -0,0 +1,28 @@
+weather = "sunny"
+if weather == "sunny":
+ print("Wear sunglasses!")
+else:
+ print("Take an umbrella!")
+
+user_mood = input("How do you feel today? ")
+if user_mood == "happy":
+ print("That's great to hear!")
+elif user_mood == "sad":
+    print("I hope your day gets better!")
+
+pi_value = 3.14
+user_age = 25
+user_location = "New York"
+MAX_LIMIT = 1000
+
+melon_price = 10
+milk_price = 3
+chips_price = 2
+total_cost = melon_price + milk_price + chips_price
+print("The total cost is: $") + str(total_cost)
+
+
+ 
+
+
+
